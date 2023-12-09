10cff8e00420c7a642906f5d7f0700e1543ef520
Sangram_29 <sangramscience29@gmail.com>
2023-12-09 14:32:46 +0530

    fixing errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
+const ignoreBuildErrors = process.env.VITE_IGNORE_BUILD_ERRORS === 'true';
 
-// Ignore build errors
-process.env.VITE_IGNORE_BUILD_ERRORS = 'true';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+  build: {
+    // Other build configurations...
+
+    // Conditionally ignore build errors
+    rollupOptions: {
+      onwarn: (warning, rollupWarn) => {
+        if (ignoreBuildErrors) {
+          return;
+        }
+        rollupWarn(warning);
+      },
+    },
+  },
+});
